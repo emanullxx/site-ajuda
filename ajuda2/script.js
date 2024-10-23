09ca73113da685636186407f705915644e898f89
emmanuel <emmanuelmoreirax@gmail.com>
2024-10-22 21:22:44 -0300

    Add files via upload

---
+++ ajuda2/script.js
@@ -0,0 +1,8 @@
+function contatoSuporte() {
+    alert("Você pode contatar o suporte pelo email: suporte@techzone.com.br");
+}
+
+function iniciarChat() {
+    alert("Iniciando o chat online...");
+    // Aqui você poderia integrar uma ferramenta de chat real.
+}
